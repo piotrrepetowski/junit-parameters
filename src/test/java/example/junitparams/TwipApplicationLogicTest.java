9a548daa951128977b0836ef84f8478d72c58ab1
Piotr Repetowski <piotr.repetowski@gmail.com>
2014-06-15 15:23:07 +0200

    Twip example

--- src/test/java/example/junitparams/TwipApplicationLogicTest.java
+++ src/test/java/example/junitparams/TwipApplicationLogicTest.java
@@ -1,20 +1,35 @@
 package example.junitparams;
 
 import net.sf.twip.TwiP;
-import static org.junit.Assert.*;
+import net.sf.twip.Values;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(TwiP.class)
 public class TwipApplicationLogicTest {
 
+    public static final Integer[] A_VALUES = {1, 0, 2, 1};
+    public static final Integer[] B_VALUES = {1, 0, 1, 2};
+
+    public static final String[] EMPTY_VALUES = {null, "", "   "};
+    public static final String[] NON_EMPTY_VALUES = {"foo", "  foo  "};
+
     @Test
-    public void testAdd(int a, int b) {
+    public void testAdd(@Values("A_VALUES") Integer a, @Values("B_VALUES") Integer b) {
+        // Not allow to provided values for both parameters, helper object must be used
         assertEquals(a + b, ApplicationLogic.add(a, b));
     }
 
-//    @Test
-//    public void testIsEmptyString(String input, boolean expected) {
-//        assertEquals(expected, ApplicationLogic.isEmptyString(input));
-//    }
+    @Test
+    public void testIsEmptyStringWithEmptyValues(@Values("EMPTY_VALUES") String input) {
+        assertTrue(ApplicationLogic.isEmptyString(input));
+    }
+
+    @Test
+    public void testIsEmptyStringWithNonEmptyValues(@Values("NON_EMPTY_VALUES") String input) {
+        assertFalse(ApplicationLogic.isEmptyString(input));
+    }
 }
